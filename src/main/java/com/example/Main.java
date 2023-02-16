ac4629ba1780198dc53c604f2580c1b8099747bf
Lars Kaulen <lars.kaulen@outlook.de>
2023-02-16 08:29:37 +0100

    Change netty deployment startup to showcase issue with second event loop group

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import org.jboss.resteasy.core.ResteasyDeploymentImpl;
-import org.jboss.resteasy.plugins.server.netty.NettyJaxrsServer;
+import org.jboss.resteasy.core.SynchronousDispatcher;
+import org.jboss.resteasy.plugins.server.netty.RequestDispatcher;
+import org.jboss.resteasy.plugins.server.netty.RequestHandler;
+import org.jboss.resteasy.plugins.server.netty.RestEasyHttpRequestDecoder;
+import org.jboss.resteasy.plugins.server.netty.RestEasyHttpResponseEncoder;
 import org.jboss.resteasy.spi.ResteasyDeployment;
 
 import java.util.ArrayList;
@@ -16,13 +30,31 @@ public class Main {
         final ResteasyDeployment deployment = new ResteasyDeploymentImpl();
         deployment.setResources(resources);
 
-        NettyJaxrsServer netty = new NettyJaxrsServer();
-        netty.setDeployment(deployment);
-        netty.setPort(8080);
-        netty.setRootResourcePath("");
-        netty.setSecurityDomain(null);
+        deployment.start();
+
+        RequestDispatcher dispatcher = new RequestDispatcher((SynchronousDispatcher) deployment.getDispatcher(), deployment.getProviderFactory(), null);
+
+        RestEasyHttpRequestDecoder restEasyHttpRequestDecoder = new RestEasyHttpRequestDecoder(dispatcher.getDispatcher(),
+                "", RestEasyHttpRequestDecoder.Protocol.HTTP);
+        RestEasyHttpResponseEncoder restEasyHttpResponseEncoder = new RestEasyHttpResponseEncoder();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ChannelPipeline pipe = ch.pipeline();
+                        pipe.addLast(new HttpRequestDecoder())
+                                .addLast(new HttpResponseEncoder())
+                                .addLast(new HttpObjectAggregator(10 * 1024 * 1024))
+                                .addLast(restEasyHttpRequestDecoder)
+                                .addLast(restEasyHttpResponseEncoder)
+                                .addLast(new NioEventLoopGroup(), new RequestHandler(dispatcher));
+                    }
+                }).childOption(ChannelOption.SO_KEEPALIVE, true);
 
         System.out.println("Starting server");
-        netty.start();
+        serverBootstrap.bind(8080).syncUninterruptibly();
     }
 }
